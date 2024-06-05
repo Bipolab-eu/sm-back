5c84e87bc048c13b728974cc8fc1431e6fa9a022
Bentagai <bentagai@protonmail.com>
2024-06-05 16:47:24 +0100

    first commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,33 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface DataAnswer extends Schema.Component {
+  collectionName: 'components_data_answers';
+  info: {
+    displayName: 'Answer';
+  };
+  attributes: {
+    answer: Attribute.String & Attribute.Required;
+    value: Attribute.Integer & Attribute.Required;
+  };
+}
+
+export interface DataQuestion extends Schema.Component {
+  collectionName: 'components_data_questions';
+  info: {
+    displayName: 'Question';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    answer: Attribute.Component<'data.answer', true>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'data.answer': DataAnswer;
+      'data.question': DataQuestion;
+    }
+  }
+}
